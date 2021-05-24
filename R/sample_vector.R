ca4eb8a235b00a19156dd7b78fb64fabd8c4a8c3
Chris Beeley <chris.beeley@gmail.com>
2021-05-24 10:40:21 +0100

    Add example

--- R/sample_vector.R
+++ R/sample_vector.R
@@ -9,6 +9,10 @@
 #' with which you want to sample
 #' @param length integer- how long you want the return value to be- e.g. the
 #' number of rows in a dataframe
+#' @examples
+#' sample_vector(values = c(NA, "Male", "Female", "Other"),
+#' weights = c(10, 50, 50, 2),
+#' length = 100)
 #'
 #' @export
 sample_vector <- function(values, weights, length){
@@ -16,6 +20,4 @@ sample_vector <- function(values, weights, length){
   sample(values, length, replace = TRUE, prob = weights / sum(weights))
 }
 
-sample_vector(values = c(NA, "Male", "Female", "Other"),
-              weights = c(10, 50, 50, 2),
-              length = 100)
+
