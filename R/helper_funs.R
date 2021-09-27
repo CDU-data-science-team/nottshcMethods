d1193d28c79f09c1e632acf6506221b15abef704
Zoe Turner <zoe.turner2@nottshc.nhs.uk>
2021-09-27 16:45:25 +0100

    Changes required for devtools::check()

--- R/helper_funs.R
+++ R/helper_funs.R
@@ -5,30 +5,33 @@
 #' Age values lower than this will be returned as missing values (NA)
 #' @param max Numeric, specifying the upper end of the last age group
 #' @param by Numeric, increment of the age categories
-#' @param method String, specyfing the method to be used for grouping age into categories.
+#' @param grouping_method String, specifying the method to be used for grouping age into categories.
 #' Details about the different methods are available here:
 #' "user_defined", "ons_1", "ons_2", "nhs_survey" ... TODO
 #'
 #'
-#' @return
 #' @export
 #' @examples
 #' # Example using a vector:
 #' set.seed(123)
-#' age <- sample(1:100, 100, replace = T)
+#' age <- sample(1:100, 100, replace = TRUE)
 #' as_age_groups(age)
 #'
 #' # Example using a data frame
-#' tibble::tibble(age = sample(1:115, 100, replace = T)) %>%
+#' tibble::tibble(age = sample(1:115, 100, replace = TRUE)) %>%
 #'   dplyr::mutate(age_groups = as_age_groups(age, min = 10, max = 100))
 as_age_groups <- function(var, min = 0, max = 100, by = 10,
-                          grouping_method = c("user_defined", "ons_1", "ons_2", "nhs_survey")) {
+                          grouping_method = c("user_defined",
+                                              "ons_1",
+                                              "ons_2",
+                                              "nhs_survey")) {
 
   grouping_method <- match.arg(grouping_method)
 
   if (grouping_method != "user_defined") {
 
-    message(paste0("The age groupings are based on guidelines defined in: '", grouping_method, "'."))
+    message(paste0("The age groupings are based on guidelines defined in: '",
+                   grouping_method, "'."))
     message(paste0("User defined arguments (`min`, `max`, and `by`) will be overwritten."))
 
   }
@@ -55,14 +58,19 @@ as_age_groups <- function(var, min = 0, max = 100, by = 10,
 #' @param data Data
 #' @param by Grouping variables used in group_by
 #' @param date_var_name String specifying date variable, needs to be class date
-#' @param .drop_latest_month Logical, specifying whether or not to drop the most recent month
-#' @param .calc_year_month_day_vars Logical, specifying whether to calculate separate year, month, and day variable
+#' @param .drop_latest_month Logical, specifying whether or not to drop the most
+#' recent month
+#' @param .calc_year_month_day_vars Logical, specifying whether to calculate
+#' separate year, month, and day variable
 #'
-#' @return
 #' @export
 #'
 #' @examples
-calc_monthly_freq <- function(data, by, date_var_name, .drop_latest_month = TRUE, .calc_year_month_day_vars = TRUE) {
+calc_monthly_freq <- function(data,
+                              by,
+                              date_var_name,
+                              .drop_latest_month = TRUE,
+                              .calc_year_month_day_vars = TRUE) {
 
   data <- data %>%
     dplyr::mutate(floor_date_m = lubridate::floor_date( {{date_var_name}} ,
@@ -85,15 +93,17 @@ calc_monthly_freq <- function(data, by, date_var_name, .drop_latest_month = TRUE
   if (.calc_year_month_day_vars) {
 
     data <- data %>%
-      mutate(year = lubridate::year(floor_date_m),
-             month = lubridate::month(floor_date_m,
-                                      label = TRUE,
-                                      abbr = TRUE),
-             day = lubridate::wday(floor_date_m,
-                                   label = TRUE,
-                                   abbr = TRUE)) %>%
+      dplyr::mutate(year = lubridate::year(floor_date_m),
+                    month = lubridate::month(floor_date_m,
+                                             label = TRUE,
+                                             abbr = TRUE),
+                    day = lubridate::wday(floor_date_m,
+                                          label = TRUE,
+                                          abbr = TRUE)) %>%
       dplyr::relocate(floor_date_m) %>%
-      dplyr::relocate(year, month, day,
+      dplyr::relocate(year,
+                      month,
+                      day,
                       .after = floor_date_m)
 
   }
